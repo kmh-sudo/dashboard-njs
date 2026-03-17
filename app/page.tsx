dc909d8e3e2b62bd9bcab508a87b8a679dbd1e77
mtdev <kyawminhtet.1dev@gmail.com>
2026-03-17 21:07:45 +0700

    fix all

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import styles from 'app/ui/home.module.css'
 import Link from 'next/link';
 import Image from 'next/image';
 
